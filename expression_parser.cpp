0892bd5b0703b78a500f85524f8db3ff94bc7ee2
Brett Stahlman <brettstahlman@gmail.com>
2020-09-06 11:17:37 -0400

    Got basic repl working, but expression grammar has an associativity problem.

--- expression_parser.cpp
+++ expression_parser.cpp
@@ -2,19 +2,25 @@
 #include <map>
 #include "parser.h"
 
+using namespace std;
+
+void run_repl()
+{
+	string line;
+	symtbl syms;
+
+	while (getline(cin, line)) {
+		Lexer lxr{line};
+		Parser p{lxr, syms};
+		auto res = p();
+		cout << ">> " << res << "\n% ";
+		line.clear();
+	}
+}
+
 int main()
 {
-	symtbl syms {{"foo", 42}, {"baz", 100}, {"bammo", 10}};
-	string s = "blooey = foo+5-10 *  (baz+(foo / bammo))";
-	//string s = "7+5-10 *  (3+(6 / 2))";
-
-	Lexer lxr{s};
-	Parser p{lxr, syms};
-	cout << "Expression: " << s << endl;
-	cout << "Result=" << p() << endl;
-	cout << "-- Final Symbol table --" << endl;
-	for (auto el : syms)
-		cout << el.first << ": " << el.second << endl;
+	run_repl();
 
 	return 0;
 }
